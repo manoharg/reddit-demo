1849d34c0ffc4576fbc2075a8093e0f2e7e9c072
manoharg <golamanohar.gm@gmail.com>
2020-05-24 10:42:36 +0530

    First commit

---
+++ src/test/java/com/manohar/redditdemo/RedditDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manohar.redditdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
